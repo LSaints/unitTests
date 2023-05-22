940b615117f30fd549bdfea4293298ce4d6ff922
LSaints <mateus.dev@gmail.com>
2023-05-22 12:21:17 -0300

    criado testes para StringAssert

---
+++ UnitTestsExample/UnitTestExampleTest/StringAssertTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Text.RegularExpressions;
+
+namespace UnitTestExampleTest
+{
+    [TestClass]
+    public class StringAssertTest
+    {
+        [TestMethod]
+        public void ContainsTest()
+        {
+            string str1 = "Mateus Leandro";
+            string str2 = "Leandro";
+
+            StringAssert.Contains(str1, str2);
+        }
+
+        [TestMethod]
+        public void StartWithTest()
+        {
+            string str1 = "Todos caixa alta";
+            string str2 = "Todos caixa";
+
+            StringAssert.StartsWith(str1, str2);
+        }
+
+        [TestMethod]
+        public void IsAllLowerCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.Matches("todos caixa", reg);
+        }
+
+        [TestMethod]
+        public void IsNotLowerCaseTest()
+        {
+            Regex reg = new Regex(@"^([^A-Z])+$");
+
+            StringAssert.DoesNotMatch("Todos Caixa", reg);
+        }
+    }
+}
